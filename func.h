ce943d882c75b964c45daee919f61194b4ee0311
kerwinchen31 <33487799+kerwinchen31@users.noreply.github.com>
2018-12-04 02:21:25 -0500

    Create func.h

---
+++ func.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+char ** parse_args( char *linee);
+
+char ** space_args( char *line);
+
+void redirect_out(char ** args, int symbol);
+
+void redirect_in(char ** args, int symbol);
